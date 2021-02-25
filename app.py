29a38a6314294caf1d62dbd49202a8fe594be25e
gitsusan <svfarago@gmail.com>
2021-02-24 17:18:28 -0700

    API Static routes complete.

--- app.py
+++ app.py
@@ -48,11 +48,12 @@ def precipitation():
     # Create session from Python to the DB
     session = Session(engine)
 
-    # Query all precipitation data
-    results = session.query(Measurement.date, Measurement.prcp).all()
+    # Query last year of precipitation data
+    results = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= "2016-08-24").\
+    filter(Measurement.date <= "2017-08-23").all()
 
-    #POTATO - is this needed here? >> session.close()
-    
+
     precipitation_data = []
     for date, prcp in results:
         precipitation_dict = {}
@@ -62,4 +63,58 @@ def precipitation():
 
     return jsonify(precipitation_data)
 
+    session.close()
+
+#------------------------------------------------
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create session from Python to the DB
+    session = Session(engine)
+
+    # Query all stations in data; use np.ravel to convert to a list
+    results = session.query(Station.station).all()
+    station_data = list(np.ravel(results))
+    return jsonify(station_data)
+
+    session.close()
+
+
+#------------------------------------------------
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create session from Python to the DB
+    session = Session(engine)
+
+    # Query most active station for the last year of data
+    results = session.query(Station.station, Measurement.date, Measurement.tobs).\
+    filter(Measurement.date >= "2016-08-24").\
+    filter(Measurement.date <= "2017-08-23").\
+    filter(Station.station == "USC00519281").all()
+   
+    tobs_data = list(np.ravel(results))
+    return jsonify(tobs_data)
+
+    session.close()
+
+
+#------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__": app.run(debug=True)
+
+
+
+
